0bdc48a7fcaac36f668c045a578b5704e20661e4
Alex Kozadaev <akozadaev at yahoo com>
2019-03-21 11:56:06 +0000

    fix error message

--- cmd.c
+++ cmd.c
@@ -12,7 +12,7 @@ cmd_run(const char *cmd)
 {
     int rv = system(cmd);
     if (rv < 0) {
-        LOG_PERROR("===== system =====]");
+        LOG_PERROR("[===== system =====]");
     } else {
         LOG_INFO_VA("[====== %s (exit code %d) =====]",
                 rv == 0 ? "ok" : "fail", rv);
